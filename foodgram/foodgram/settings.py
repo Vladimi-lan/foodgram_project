dcc1244d2825493c3e94dcc0a14f7766b5eae8bf
Vladimi <ovladimir848@gmail.com>
2022-07-22 16:42:04 +0300

    деплой

--- foodgram/foodgram/settings.py
+++ foodgram/foodgram/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '[::1]', 'web', '207.180.240.196']
 
 
 # Application definition
