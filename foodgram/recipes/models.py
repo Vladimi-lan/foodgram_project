4d47724487aa8343321f09a9d56384786dd2b5af
Vladimi <ovladimir848@gmail.com>
2022-07-25 11:01:01 +0300

    Модель Recipe.tags

--- foodgram/recipes/models.py
+++ foodgram/recipes/models.py
@@ -68,7 +68,7 @@ class Recipe(models.Model):
     )
     tags = models.ManyToManyField(
         Tag,
-        # through='TagRecipe',
+        through='TagRecipe',
         related_name='recipes',
         verbose_name='Теги рецепта',
         help_text='Выберите подходящие теги'
