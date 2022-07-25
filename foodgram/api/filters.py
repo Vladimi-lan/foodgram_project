9006ca197f20a8a9874e5eef3176e82d06b44817
Vladimi <ovladimir848@gmail.com>
2022-07-25 15:07:21 +0300

    Фильтрация по тегам. Проверка

--- foodgram/api/filters.py
+++ foodgram/api/filters.py
@@ -1,4 +1,4 @@
-from django_filters import rest_framework as django_filter
+import django_filters
 from recipes.models import Recipe
 from rest_framework import filters
 from users.models import CustomUser
@@ -8,11 +8,11 @@ class IngredientSearchFilter(filters.SearchFilter):
     search_param = 'name'
 
 
-class RecipeFilters(django_filter.FilterSet):
-    author = django_filter.ModelChoiceFilter(queryset=CustomUser.objects.all())
-    tags = django_filter.AllValuesMultipleFilter(field_name='tags__slug')
-    is_favorited = django_filter.BooleanFilter(method='get_is_favorited')
-    is_in_shopping_cart = django_filter.BooleanFilter(
+class RecipeFilters(django_filters.FilterSet):
+    author = django_filters.ModelChoiceFilter(queryset=CustomUser.objects.all())
+    tags = django_filters.AllValuesMultipleFilter(field_name='tags__slug')
+    is_favorited = django_filters.BooleanFilter(method='get_is_favorited')
+    is_in_shopping_cart = django_filters.BooleanFilter(
         method='get_is_in_shopping_cart')
 
     class Meta:
